9884632894b7b79195f15b122cbfbae30bdfbd2a
荣顶 <wangrongding@qq.com>
2021-07-29 18:23:19 +0800

    hold: 暂存

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="home">
-        <img alt="Vue logo" src="../assets/logo.png" />
-        <HelloWorld msg="Welcome to Your Vue.js App" />
+        <img alt="Vue logo" id="img" src="../assets/logo.png" />
+        <canvas id="canvas"></canvas>
     </div>
 </template>
 
@@ -13,6 +13,18 @@ export default {
     name: "Home",
     components: {
         HelloWorld,
+    },
+    mounted() {
+        let img = document.getElementById("img");
+        let canvas = document.getElementById("canvas");
+        canvas.setAttribute("width", img.width);
+        canvas.setAttribute("height", img.height);
+        let context = canvas.getContext("2d");
+        // 将图片画在画布上
+        context.drawImage(img, 0, 0);
+        // 获取像素数据
+        let imgData = context.getImageData(0, 0, img.width, img.height);
+        let pixelData = imgData.data;
     },
     methods: {},
 };
