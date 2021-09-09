4abfe842b6d793db34a2e902eb48ec866c125e79
荣顶 <wangrongding@qq.com>
2021-09-09 18:27:03 +0800

    feat: 新增项目gitpage

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const resolve = (dir) => path.join(__dirname, dir);
 module.exports = {
+    publicPath: "./",
     outputDir: "docs", // 输出文件目录
     devServer: {
         port: 9425,
