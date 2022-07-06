ddbf1ce7261920be7fd08c65116b49d7d2093c4c
wangrongding <wangrongding@qq.com>
2022-07-06 18:29:04 +0800

    fix: 取消阻止默认事件

--- src/layout/layout.vue
+++ src/layout/layout.vue
@@ -4,19 +4,19 @@ import Menu from '@/layout/Menu.tsx'
 
 onMounted(() => {
   // 禁止右键菜单
-  document.oncontextmenu = () => false
-  // oncopy事件禁用复制;
-  document.oncopy = () => false
-  // oncut事件禁用剪切
-  document.oncut = () => false
-  // onselectstart事件禁用网页上选取的内容
-  document.onselectstart = () => false
+  // document.oncontextmenu = () => false
+  // // oncopy事件禁用复制;
+  // document.oncopy = () => false
+  // // oncut事件禁用剪切
+  // document.oncut = () => false
+  // // onselectstart事件禁用网页上选取的内容
+  // document.onselectstart = () => false
 })
 </script>
 <template>
   <div class="page-container">
     <Menu />
-    <div class="content">
+    <div id="content" class="content">
       <router-view />
       <!-- <router-view><router-view /></router-view> -->
     </div>
@@ -25,15 +25,17 @@ onMounted(() => {
 
 <style scoped lang="scss">
 .page-container {
-  user-select: none;
-  display: flex;
-  flex-direction: column;
+  // user-select: none;
+  // display: flex;
+  // flex-direction: column;
   height: 100vh;
 
   .content {
+    position: relative;
     background: #e8f1ff;
     overflow-y: auto;
-    flex: 1;
+
+    // flex: 1;
   }
 }
 </style>
