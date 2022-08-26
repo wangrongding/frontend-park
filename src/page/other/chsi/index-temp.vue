a7effd32385b3b35bb2188874e71ec5704134081
wangrongding <wangrongding@qq.com>
2022-08-26 10:40:02 +0800

    chore: 修改chsi说明文本

--- src/page/other/chsi/index-temp.vue
+++ src/page/other/chsi/index-temp.vue
@@ -1,10 +1,21 @@
 <script setup lang="ts"></script>
 <template>
-  <div class="page-container">暂时关闭了。。。</div>
+  <div class="page-container">
+    <p>暂时关闭了。。。</p>
+    <p>
+      这影响到他人利益，暂时不公开了，如果有迫切需要的话，私聊我发链接给你。
+    </p>
+  </div>
 </template>
 <style lang="scss" scoped>
 .page-container {
   text-align: center;
-  padding: 20px;
+  padding-top: 200px;
+
+  p {
+    margin: 0 auto;
+    text-align: left;
+    width: 800px;
+  }
 }
 </style>
